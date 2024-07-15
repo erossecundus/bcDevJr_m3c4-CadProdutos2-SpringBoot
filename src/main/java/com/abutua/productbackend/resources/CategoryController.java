b2624fc36754dc3f78a25dd203ed65ed132ab6df
Eros Secundus <erossecundus@gmail.com>
2024-07-15 17:24:55 -0300

    Camadas de Serviço de Product e Category

--- src/main/java/com/abutua/productbackend/resources/CategoryController.java
+++ src/main/java/com/abutua/productbackend/resources/CategoryController.java
@@ -3,16 +3,14 @@ package com.abutua.productbackend.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.productbackend.models.Category;
-import com.abutua.productbackend.repositories.CategoryRepository;
+import com.abutua.productbackend.services.CategoryService;
 
 @RestController
 @CrossOrigin
@@ -20,21 +18,19 @@ public class CategoryController {
 
   // Anotation para injeção de dependência
   @Autowired
-  private CategoryRepository categoryRepository;
+  private CategoryService categoryService;
 
-  //criar endpoint de categoria buscada
+  // criar endpoint de categoria buscada
   @GetMapping("categories/{id}")
   public ResponseEntity<Category> getCategory(@PathVariable int id){
-    Category category = categoryRepository.findById(id)
-                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
-
+    Category category = categoryService.getById(id);
     return ResponseEntity.ok(category);
   }
 
-  //criar endpoint de categorias
+  // criar endpoint de categorias
   @GetMapping("categories")
   public List<Category> getCategories(){
-    return categoryRepository.findAll();
+    return categoryService.getAll();
   }
 
 }
